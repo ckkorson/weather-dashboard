f3b5f81cfa27a90eeb0b586869bbd77a2a33492d
Caleb Korson <caleb@Calebs-Air.attlocal.net>
2022-09-10 10:08:50 -0400

    added weather image and second fetch for forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,18 +12,39 @@ function getApi() {
       })
     .then(function(data) {
         console.log(data)
-        console.log(data.name)
+        // console.log(data.name)
         currentWeather(data)
+        let cityLat = data.coord.lat
+        let cityLon = data.coord.lon
+        weatherForecast(cityLat,cityLon)
     })
 }
 
 function currentWeather(data) {
+    let weatherImg = 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png'
     document.getElementById('city').innerHTML = data.name
-    document.getElementById('temp').innerHTML = 'Temp: ' + data.main.temp + '°F'
-    document.getElementById('wind').innerHTML = 'Wind: ' + data.wind.speed + ' mph'
+    document.getElementById('city-img').setAttribute('src', weatherImg)
+    // let cityTemp = Math.round(data.main.temp)
+    // let cityTempArr = cityTemp.split(".")
+    document.getElementById('temp').innerHTML = 'Temp: ' + Math.round(data.main.temp) + '°F'
+    document.getElementById('wind').innerHTML = 'Wind: ' + Math.round(data.wind.speed) + ' mph'
     document.getElementById('humidity').innerHTML = 'Humidity: ' + data.main.humidity + '%'
+    document.getElementById('feels-like').innerHTML = 'Feels like: ' + Math.round(data.main.feels_like) + '°F'
+}
+
+function weatherForecast(cityLat,cityLon) {
+    let forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon + '&appid=dd267f7775731528762e4ab72ff77aef'
+    fetch(forecastUrl)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
 }
 
 searchBtn.addEventListener('click', function(){
     getApi()
 })
+
+getApi()
